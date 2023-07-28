bba7f53b710bd65a36c95f0898ac0e995ecbd4f9
nomayen <nomayen.ohin@gmail.com>
2023-07-28 12:02:34 +0600

    navbar add

---
+++ src/layout/Main.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer/Footer";
+import NabBar from "../Shared/NabBar/NabBar";
+
+const Main = () => {
+    return (
+        <div>
+            <NabBar></NabBar>
+           <Outlet></Outlet>
+           <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
