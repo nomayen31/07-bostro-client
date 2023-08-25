4779ecf3b290f4385c9a257bf0fc840b38710865
nomayen <nomayen.ohin@gmail.com>
2023-08-25 19:18:46 +0600

    order handeler

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,7 +1,42 @@
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+
 const FoodCard = ({item}) => {
     const {image, price, recipe, name }= item;
+    const {user}= useContext(AuthContext);
+    const navigate = useNavigate();
     const handleAddToCart = item =>{
         console.log(item);
+        if (user) {
+            fetch('http://localhost:5000/carts')
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    Swal.fire(
+                        'Good job!',
+                        'You clicked the button!',
+                        'success'
+                      ) 
+                }
+            })
+        }
+        else{
+            Swal.fire({
+                title: 'Please login to order food',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'login Now'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  navigate('/login')
+                }
+              })
+        }
 
     }
     return (
