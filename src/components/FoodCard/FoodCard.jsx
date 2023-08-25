f96ad29abd407f5617b8295fcfec301c3f8f5994
nomayen <nomayen.ohin@gmail.com>
2023-08-25 09:07:27 +0600

    order counte navbar add

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,5 +1,9 @@
 const FoodCard = ({item}) => {
     const {image, price, recipe, name }= item;
+    const handleAddToCart = item =>{
+        console.log(item);
+
+    }
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure><img src={image} alt="Shoes" /></figure>
@@ -8,7 +12,7 @@ const FoodCard = ({item}) => {
                 <h2 className="card-title text-center">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-outline border-0 border-b-4 mt-4 bg-slate-100 border-orange-400">Add to Cart</button>
+                    <button onClick={() =>handleAddToCart(item)} className="btn btn-outline border-0 border-b-4 mt-4 bg-slate-100 border-orange-400">Add to Cart</button>
                 </div>
             </div>
         </div>
